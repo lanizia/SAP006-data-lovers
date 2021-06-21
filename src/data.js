6ac377e008ebca7045babb9fa96ec45ee9b9b8b2
tenorionique <monique_doretto@hotmail.com>
2021-06-21 16:20:38 -0300

    atualização de cards e fonts

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 import data from './data/athletes/athletes.js'
 
-export const obterAtletas = () => data.athletes.slice(0, 10)
+export const obterAtletas = () => data.athletes.slice(0, 9)
 
 export const obterAtletasPorNome = (athleteName) => {
   const lowerCaseName = athleteName.toLowerCase();
-  return data.athletes.filter(atleta => atleta.name.toLowerCase().startsWith(lowerCaseName)).slice(0, 10);
+  return data.athletes.filter(atleta => atleta.name.toLowerCase().startsWith(lowerCaseName)).slice(0, 9);
 }
 
 
