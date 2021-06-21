b97c95214f1e72d94bc3d2b403cf29f8b582777d
tenorionique <monique_doretto@hotmail.com>
2021-06-21 16:02:51 -0300

    alterações de cards e fontes

--- src/main.js
+++ src/main.js
@@ -8,14 +8,18 @@ const imprimirAtletas = (atletas) => {
         cardsElement.innerHTML += `<div class="card">
         <div class="topCard">
             <h2 class="title">${atleta.name}</h2>
-            <span class="secondText"> Exemplo de texto secundário</span> <!--Usou span pois não tem margens como a tag de paragrafo-->
+            <span class="secondText"><b>Gênero:</b> ${atleta.gender} <b>Idade:</b> ${atleta.age} <b>País:</b> ${atleta.team} </span> <!--Usou span pois não tem margens como a tag de paragrafo-->
         </div>
         <div class="mediaCard"></div>
         <div class="bottomCard">
-            <p class="bottomText">Aqui é um texto exemplo de resumo da página ou documento que o card trata</p>
+            <p class="bottomText">
+            <b>Altura:</b> ${atleta.height} cm
+            <b>Peso:</b> ${atleta.weight} kg</br>
+            <b>Esporte:</b> ${atleta.sport}</br>
+            <b>Modalidade:</b> ${atleta.event}</br>
+            <b>Medalha:</b> ${atleta.medal}</p>
             <div class="actionsCard">
-                <button class="actions">Curtir</button>
-                <button class="actions">Compartilhar</button>
+                
             </div>
         </div>
         </div>`;
