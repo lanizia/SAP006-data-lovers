f6897701b704b44dbb486936f03e00a1f9e91791
tenorionique <monique_doretto@hotmail.com>
2021-06-24 10:27:26 -0300

    paises atualizados

--- src/data.js
+++ src/data.js
@@ -1,6 +1,40 @@
+
 export const showAthletes = (athletesList) => athletesList.slice(0, 9);
 
 export const filterByName = (athletesList, athleteName) => {
   const lowerCaseName = athleteName.toLowerCase();
   return athletesList.filter(athlete => athlete.name.toLowerCase().startsWith(lowerCaseName)).slice(0, 9);
 }
+
+
+export const getTeams = (athletesList) => {
+  const teamOfAthletes = athletesList.map(athlete => athlete.team)
+  const teamNewList = [...new Set(teamOfAthletes)];
+  return teamNewList;
+}
+
+
+export const sortTeams = (listTeamsAz) => {
+  const order = listTeamsAz.sort((a, b) => (a < b ? -1 : a > b ? 1: 0));
+  const teamSortOrder = [...new Set(order)];
+  return teamSortOrder;
+};
+
+
+
+ /* export const teamsNotDuplicated = new Set();
+
+data.athletes.forEach((athletes) => {
+    teamsNotDuplicated.add(athletes.team)
+});
+
+const arrayTeam = [...teamsNotDuplicated];
+
+arrayTeam.sort();
+console.log(arrayTeam.sort()); */
+
+
+ 
+
+
+ 
--- src/main.js
+++ src/main.js
@@ -1,44 +1,71 @@
 import data from './data/athletes/athletes.js'
-import { showAthletes, filterByName } from './data.js';
+import { showAthletes, filterByName, getTeams, sortTeams } from './data.js';
 
-const printAthletes = (athletesList) => {
-    const cardsElement = document.getElementById("cards");
-    cardsElement.innerHTML = "";
 
-    athletesList.forEach(athlete => {
-        cardsElement.innerHTML += `<div class="card">
-        <div class="topCard">
-            <h2 class="title">${athlete.name}</h2>
-            <span class="secondText"><b>Gênero:</b> ${athlete.gender} <b>Idade:</b> ${athlete.age} <b>País:</b> ${athlete.team} </span> <!--Usou span pois não tem margens como a tag de paragrafo-->
-        </div>
-        <div class="mediaCard"></div>
-        <div class="bottomCard">
-            <p class="bottomText">
-            <b>Altura:</b> ${athlete.height} cm
-            <b>Peso:</b> ${athlete.weight} kg</br>
-            <b>Esporte:</b> ${athlete.sport}</br>
-            <b>Modalidade:</b> ${athlete.event}</br>
-            <b>Medalha:</b> ${athlete.medal}</p>
-            <div class="actionsCard">
+const btnTeam = document.getElementById("team");
+
+btnTeam.addEventListener("click", () => {
+    cardsTeam.innerHTML = "";
+    const listTeams= getTeams(data.athletes);
+    printTeams(listTeams);
+})
+
+const cardsTeam = document.getElementById("team-section");
+const printTeams = (athletesList) => {
+    
+        cardsTeam.innerHTML = "";
+
+        athletesList.forEach(listTeams => {
+            cardsTeam.innerHTML += `<section class="base"> 
+            <section class="container-two">
+            <div class="row-two">
+                <div class="card-two">
+                    <div class="card-two-header">
+                        <h1>${listTeams}</h1>
+                    </div>
+                    <div class="card-two-body">
+                        <p></p>
+                    </div>
+                </div>
+                </section>`})}
+
+
+                const printAthletes = (athletesList) => {
+                    const cardsAthlete = document.getElementById("cards");
+                    cardsAthlete.innerHTML = "";
                 
-            </div>
-        </div>
-        </div>`;
-    });
-}
-
-const athletesList = showAthletes(data.athletes)
-printAthletes(athletesList);
-
-
-const btnSearch = document.getElementById("searchAthlete");
-const athleteName = document.getElementById("athlete");
-
-btnSearch.addEventListener("click", () => {
-    const searchAthleteByName = filterByName(data.athletes, athleteName.value);
-    printAthletes(searchAthleteByName);
-} )
-
-
-
-
+                    athletesList.forEach(athlete => {
+                        cardsAthlete.innerHTML += `<div class="card">
+                        <div class="topCard">
+                            <h2 class="title">${athlete.name}</h2>
+                            <span class="secondText"><b>Gênero:</b> ${athlete.gender} <b>Idade:</b> ${athlete.age} <b>País:</b> ${athlete.team} </span> <!--Usou span pois não tem margens como a tag de paragrafo-->
+                        </div>
+                        <div class="mediaCard"></div>
+                        <div class="bottomCard">
+                            <p class="bottomText">
+                            <b>Altura:</b> ${athlete.height} cm
+                            <b>Peso:</b> ${athlete.weight} kg</br>
+                            <b>Esporte:</b> ${athlete.sport}</br>
+                            <b>Modalidade:</b> ${athlete.event}</br>
+                            <b>Medalha:</b> ${athlete.medal}</p>
+                            <div class="actionsCard">
+                                
+                            </div>
+                        </div>
+                        </div>`;
+                    });
+                }
+                    
+                
+                const athletesList = showAthletes(data.athletes)
+                printAthletes(athletesList);
+                
+                
+                const btnSearch = document.getElementById("searchAthlete");
+                const athleteName = document.getElementById("athlete");
+                
+                btnSearch.addEventListener("click", () => {
+                    const searchAthleteByName = filterByName(data.athletes, athleteName.value);
+                    printAthletes(searchAthleteByName);
+                } )
+                
